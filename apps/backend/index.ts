4538a8aed0110606dd9f8ce5b6f34f80142c83a4
Prabhanshu <prabhanshuop@gmail.com>
2026-02-22 11:35:08 +0530

    init

---
+++ apps/backend/index.ts
@@ -0,0 +1,21 @@
+import express from "express"
+
+const app = express();
+
+app.post("/api/v1/signup",(req,res)=>{
+
+});
+
+app.post("/api/v1/signin",(req,res)=>{
+
+});
+
+app.post("/api/v1/txn/sign",(req,res)=>{
+
+});
+
+app.get("/api/v1/txn",(req,res)=>{
+
+});
+
+app.listen(3000);
